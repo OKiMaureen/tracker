549036e3f0b609188f1ff4d9714e52d2bb04eded
OkiMaureen <oki.maureen.eloho@gmail.com>
2018-01-26 01:36:58 +0100

    create front end using vue.js

---
+++ client/src/components/Register.vue
@@ -0,0 +1,31 @@
+<template>
+  <div>
+<h1>
+  Register
+</h1>
+<input v-model="email" type="email" name="email" placeholder="Email">
+<br/>
+<input v-model="password" type="password" name="password" placeholder="PassWord">
+<br/>
+<button @click="register">Register</button>
+  </div>
+</template>
+
+<script>
+import authenticationService from '@/services/authenticationService'
+export default {
+  data () {
+    return { email: '', password: '' }
+  },
+  methods: {
+    async register () {
+      const response = await authenticationService.register({email: this.email, password: this.password})
+      console.log(response.data)
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
