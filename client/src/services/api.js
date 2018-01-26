e50b4aa75bfdbd0bfde04c5699e81569410de870
OkiMaureen <oki.maureen.eloho@gmail.com>
2018-01-26 15:22:53 +0100

    use axios module to set up connection to back end

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -1,7 +1,8 @@
+// set up connection to the backend
 // import axios module
 import axios from 'axios'
 
-// exporting api module
+// exporting api module (returns a connector)
 export default () => {
   return axios.create({ baseURL: `http://localhost:8081/` })
 }
